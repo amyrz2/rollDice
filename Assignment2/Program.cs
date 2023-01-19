62971451df6c49506105d21e8d4938fe23cefc72
Amy Zhang <amyzhang59@yahoo.com>
2023-01-18 19:49:20 -0700

    first commit

---
+++ Assignment2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Assignment2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            rollDice rd = new rollDice();
+
+            int diceRolls = 0;
+
+            // get dice roll count 
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            Console.WriteLine("How many dice rolls would you like to simulate? ");
+            diceRolls = Convert.ToInt32(Console.ReadLine());
+
+            rd.roll(diceRolls);
+
+            Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
+
---
+++ Assignment2/rollDice.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace Assignment2
+{
+	public class rollDice
+	{
+		public void roll(int numrolls)
+		{
+            int[] sums = new int[11];
+            Double[] percentages = new Double[11];
+
+            // roll dice twice randomly
+            Random rnd = new Random();
+
+            // rolls dice # of times indicated by user
+            for (int i = 0; i < numrolls; i++)
+            {
+                // roll 2 dice and add them up 
+                int diceOne = rnd.Next(1, 7);
+                int diceTwo = rnd.Next(1, 7);
+                int diceSum = diceOne + diceTwo;
+
+                int posInArray = diceSum - 2;
+
+                sums[posInArray]++;
+                   
+            }
+
+            Double total = 0;
+            Double percent = 0.0;
+
+            // calculate percentage and store in array 
+            for (int i = 0; i < 11; i++)
+            {
+                total = sums[i];
+
+                percent = (total/numrolls*100);
+                percent = Math.Round(percent);
+                percent = Convert.ToInt32(percent);
+
+                percentages[i] = percent;
+            }
+
+
+            // print results
+            Console.WriteLine("\n" + "DICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each ' * ' represents 1 % of the total number of rolls.");
+            Console.WriteLine("Total number of rolls = " + numrolls.ToString() + '\n');
+
+            string sStars = "";
+            Double numStars = 0;
+
+            // display histograph
+
+            for (int i = 0; i < 11; i++)
+            {
+                numStars = percentages[i];
+
+                for (int j = 0; j < numStars; j++)
+                {
+                    sStars = sStars + '*';
+                }
+                Console.WriteLine((i + 2).ToString() + ": " + sStars);
+
+                sStars = "";
+            }
+
+        }
+	}
+}
+
+// this is a commit
